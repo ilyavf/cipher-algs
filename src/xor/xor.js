29df75c757f3b7f52cb039e89f17553e2bb29099
ilyavf <fadeev.ilya@gmail.com>
2014-08-15 10:26:21 -0400

    xor: working on index of coincidence

--- src/xor/xor.js
+++ src/xor/xor.js
@@ -24,10 +24,27 @@ var charMap = function (s) {
 // breakXor :: string -> [string, key]
 var breakXor = function (xored) {
 
-}
+};
+
+var rotateString = function (s, n) {
+    n = n || 1;
+    return s.slice(n) + s.slice(0,n);
+};
+
+// :: string -> number
+var countCoincidences = function (s1, s2) {
+    var count = 0;
+    for (var i in s1) {
+        if (s1[i] === s2[i]) count++;
+    }
+    return count;
+};
 
 module.exports = {
     xorChar: xorChar,
     xorString: xorString,
-    charMap: charMap
+    charMap: charMap,
+    rotateString: rotateString,
+    countCoincidences: countCoincidences,
+    indexOfCoincidence: countCoincidences
 };
--- src/xor/xor.test.js
+++ src/xor/xor.test.js
@@ -18,8 +18,9 @@ assert.equal(
     , 'abcdefgh ^ xyz = ...');
 assert.equal(xor.xorString(xor.xorString(s1, s2), s2), s1, 'Double xor should return original string');
 assert.equal(xor.xorChar('a', 'b').charCodeAt(0), 3, '"a" XOR "b" should give 3');
-
-assert.deepEqual(xor.charMap('abc'), [97,98,99], 'Char map of "abc" should be [97,98,99]')
+assert.deepEqual(xor.charMap('abc'), [97,98,99], 'Char map of "abc" should be [97,98,99]');
+assert.equal(xor.rotateString('hello', 3), 'lohel', '"hello" rotated by 3 should be "lohel"');
+assert.equal(xor.countCoincidences('hello', 'bilbo'), 2, 'indexOfCoincidence of "hello" and "bilbo" should be 2');
 
 console.log('All tests passed. OK!');
 
@@ -29,3 +30,43 @@ console.log('All tests passed. OK!');
 //
 //console.log('a ^ x = ' + ('a'.charCodeAt(0) ^ 'x'.charCodeAt(0)))
 //console.log('c ^ z = ' + ('c'.charCodeAt(0) ^ 'z'.charCodeAt(0)))
+
+console.log('indexOfCoincidence: ' + xor.indexOfCoincidence('hello', 'bilbo'));
+console.log('rotateString: helloworld');
+console.log('rotateString: ' + xor.rotateString('helloworld', 4));
+
+var orig = 'isthereanybodygoingtolistentomystoryallaboutthegirlwhocametostay',
+    key = 'hello',
+    crypted = xor.xorString(orig, key);
+
+console.log('check: ' + xor.xorString(orig, crypted));
+
+console.log('map: ' + xor.charMap(orig));
+console.log('done1: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 1))));
+console.log('done2: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 2))));
+console.log('done3: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 3))));
+console.log('done4: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 4))));
+console.log('done5: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 5))));
+
+
+
+//console.log('crypted: ' + xor.charMap(crypted));
+//console.log('crypted: ' + crypted);
+//var rotated = xor.rotateString(crypted, 8);
+//console.log('rotated: ' + rotated);
+//console.log('coincidences: ' + xor.indexOfCoincidence(crypted, rotated));
+
+/*console.log('rotate01: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 1))));
+console.log('rotate02: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 2))));
+console.log('rotate03: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 3))));
+console.log('rotate04: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 4))));
+console.log('rotate05: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 5))));
+console.log('rotate06: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 6))));
+console.log('rotate07: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 7))));
+console.log('rotate08: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 8))));
+console.log('rotate09: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 9))));
+console.log('rotate10: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 10))));
+console.log('rotate11: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 11))));
+console.log('rotate12: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 12))));
+console.log('rotate13: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 13))));
+console.log('rotate14: ' + xor.indexOfCoincidence(crypted, xor.xorString(crypted, xor.rotateString(crypted, 14))));*/
